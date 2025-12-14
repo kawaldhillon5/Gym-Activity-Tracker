0c42b5443b465f416bbb377647556ed0fdef3612
kawal <kawaldhillon5@gmail.com>
2025-12-13 22:11:47 -0500

    updated user section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,50 @@
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth, type User } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
-
 import '../css/Navbar.css'
+import { useEffect, useRef, useState, type MouseEventHandler, type RefObject } from "react";
+import { AnimatedCheckmark } from "./AnimatedCheckMark";
+import { User2, X } from "lucide-react";
+import { useOnClickOutside } from "../contexts/OnClickOutside";
+
+type LogOutStatus = 'idle' | 'success';
+
 
 export const Navbar = () =>{
-    const { token,user, logout } = useAuth();
+
+    const { user, logout } = useAuth();
     const navigate = useNavigate();
 
+    const [isToucehd , setIsTouched] = useState<boolean>(false)
+    const [isExpanded , setIsExpanded] = useState<boolean>(false)
+    const [LogOutStatus, setLogOutStatus] = useState<LogOutStatus>("idle")
+
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    useOnClickOutside(dropdownRef, () => {
+            setIsExpanded(false)
+            setIsTouched(false)
+        }
+    );
+
+
+
     const handleLogout = () =>{
-        logout()
-        navigate('/login')
+        setLogOutStatus("success")
+        setTimeout(()=>{
+            setIsExpanded(false)
+            setIsTouched(false)
+        }, 500)
+        setTimeout(()=>{
+            logout()
+            navigate('/login')
+            setLogOutStatus("idle")
+        },1000)
+
+    }
+
+    const handleTouched = () =>{
+        setIsTouched((prev) => !prev)
+        setIsExpanded((prev) => !prev)
     }
 
     if (user == null){
@@ -18,19 +53,63 @@ export const Navbar = () =>{
 
     return (
         <nav>
-            <Link className="home_link" to={'/'}>Home</Link>
-            {
-                token && user ?
-                    <div className="auth_links">
-                        <span>{user.user_name}</span>
-                        <button onClick={handleLogout}>Log Out</button>
-                    </div>
-                    :
-                    <div className="auth_links">
-                        <Link to={"/login"}>Log In</Link> 
-                        <Link to={"/signup"}>Sign Up</Link>
-                    </div>                    
-            }
+            
+            <Link className="home_link" to={'/home'}>Home</Link>
+            <div
+                onClick={handleTouched} 
+                className={`user_link ${isToucehd ? "user_link_expanded":""}`}>
+                {isToucehd ? <X />:<User2 />}
+            </div>
+            <UserLink dropdownRef={dropdownRef} user={user} logOutstatus={LogOutStatus}  isExpanded = {isExpanded} handleLogout={handleLogout}/>
+
+                           
         </nav>
+    )
+}
+
+const UserLink = function({user, handleLogout, logOutstatus, isExpanded, dropdownRef}:{user: User, handleLogout: MouseEventHandler, logOutstatus: LogOutStatus, isExpanded: boolean, dropdownRef: RefObject<HTMLDivElement | null>}){
+    
+    const [userNameVisible, setUserNameVisible] = useState<boolean>(false)
+    const [logoutBtnVisible, setLogoutBtnVisible] = useState<boolean>(false)
+
+    useEffect(()=>{
+        if(isExpanded){
+            setTimeout(()=>{
+                setUserNameVisible(true)
+            },100)
+            setTimeout(()=>{
+                setLogoutBtnVisible(true)
+            },200)            
+        } else {
+            setTimeout(()=>{
+                setLogoutBtnVisible(false)
+            },100)
+            setTimeout(()=>{
+                setUserNameVisible(false)
+            },200)         
+        }
+    },[isExpanded])
+
+    
+    return (
+        <div 
+            ref={dropdownRef}
+            className={`user_link_div ${isExpanded ? "user_link_div_expanded":""}`}
+            style={{height: isExpanded ? "500%" : "0%", padding: isExpanded ? "60px 15px 15px 15px": "0px", border: isExpanded ? "1px solid var(--border-subtle)" : "0px" , width: isExpanded ? "40vw": "10vw"}}
+            >
+            <span 
+                className="user_drop_down_name"
+                style={{opacity: userNameVisible ? 1 : 0}}
+                ><User2/>{user.user_name}
+            </span>
+            <button
+                className=" user_drop_down_btn"
+                id={logOutstatus === "success" ? 'btn_success': ""}
+                style={{ marginTop: '10px', opacity: logoutBtnVisible ? 1 : 0, boxShadow : "0 0 20px white" }}
+                onClick={handleLogout}>
+                    {logOutstatus === 'idle' && "Log Out"}
+                    {logOutstatus === 'success' && <AnimatedCheckmark height={40} width={40} />}
+            </button>
+        </div>
     )
 }
---
+++ src/contexts/OnClickOutside.tsx
@@ -0,0 +1,21 @@
+import { useEffect, type RefObject } from 'react';
+
+export const useOnClickOutside = <T extends HTMLElement | null>(ref : RefObject<T> , handler : Function) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
